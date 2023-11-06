c1d6f0ee73d536f41ebec28ddc56f80c1e366e23
dongqian06 <dongqian06@baidu.com>
2023-11-06 19:22:21 +0800

    Init commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+################################################################################
+#
+# Copyright (c) 2023 Baidu.com, Inc. All Rights Reserved
+#
+################################################################################
+"""
+Setup script.
+
+Authors: dongqian06(dongqian06@baidu.com)
+Date:    2023/11/06 19:22:21
+"""
+
+import setuptools
+
+setuptools.setup()
+
