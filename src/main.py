4010aa8dc15f2e068079c0315c5f5030031f86bb
Deriq-Qian-Dong <471607065@qq.com>
2023-11-08 20:45:24 +0800

    update

--- src/main.py
+++ src/main.py
@@ -1,19 +1,21 @@
-from utils import *
+import os
 import torch
-from accelerate import Accelerator
-from transformers import AutoModelForCausalLM, AutoTokenizer
+import numpy as np
+from utils import *
+from tqdm import tqdm
+from time import time
 import torch_optimizer as optim
-from torch.optim.lr_scheduler import CosineAnnealingLR, LinearLR
+from accelerate import Accelerator
 from dataset_factory import DialogSFTDataset
 from torch.utils.data import DataLoader, Dataset
-from tqdm import tqdm
-import numpy as np
-import os
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from torch.optim.lr_scheduler import CosineAnnealingLR, LinearLR
 os.environ['http_proxy'] = 'http://172.19.57.45:3128'
 os.environ['https_proxy'] = 'http://172.19.57.45:3128'
 
 optimizer_class = {"AdamW": torch.optim.AdamW, "Lamb": optim.Lamb}
 scheduler_class = {"CosineAnnealingLR": CosineAnnealingLR, "LinearLR": LinearLR}
+iter_count = 0
 
 def main():
     config = get_config()
@@ -36,7 +38,7 @@ def main():
     train_dataloader = DataLoader(train_dataset, batch_size=train_config['batch_size'], shuffle=False, collate_fn=train_dataset._collate_fn)
     test_dataset = DialogSFTDataset(tokenizer, dataset_config['test'])
     test_dataloader = DataLoader(test_dataset, batch_size=train_config['batch_size'], shuffle=False, collate_fn=test_dataset._collate_fn)
-    accelerator = Accelerator()
+    accelerator = Accelerator(log_with=train_config['log_with'], project_dir=train_config['project_dir'])
     (model, optimizer, train_dataloader, test_dataloader) = accelerator.prepare(model, optimizer, train_dataloader, test_dataloader)
     test(model, tokenizer, optimizer, scheduler, test_dataloader, accelerator, 0)
     for epoch in range(1, 1+train_config['num_epochs']):
@@ -47,11 +49,23 @@ def train(model, optimizer, train_dataloader, scheduler, accelerator, epoch):
     model.train()
     step = 0
     pbar = tqdm(total=len(train_dataloader))
+    global iter_count
     for batch in train_dataloader:
+        iter_count+=1
         batch = accelerator.prepare(batch)
+        forward_time = time()
         outputs = model(**batch)
+        forward_time = time() - forward_time
         loss = outputs.loss
+        stats = {"loss": loss}
+        backward_time = time()
         accelerator.backward(loss)
+        backward_time = time() - backward_time
+        stats["time/forward"] = forward_time
+        stats["time/backward"] = backward_time
+        for group_number, lr in enumerate(scheduler.get_last_lr()):
+            stats[f"learning_rate_group_{group_number}"] = lr
+        accelerator.log(stats, step=iter_count)
         optimizer.step()
         scheduler.step()
         optimizer.zero_grad()
@@ -64,6 +78,7 @@ def train(model, optimizer, train_dataloader, scheduler, accelerator, epoch):
 def test(model, tokenizer, optimizer, scheduler, test_dataloader, accelerator, epoch):
     model.eval()
     total_loss = 0
+    global iter_count
     with torch.no_grad():
         for batch in tqdm(test_dataloader, desc=f"Evaluation of epoch {epoch}"):
             batch = accelerator.prepare(batch)
@@ -76,6 +91,8 @@ def test(model, tokenizer, optimizer, scheduler, test_dataloader, accelerator, e
     accelerator.print(f"Epoch {epoch} | Perplexity: {perplexity:.4f} | Loss: {total_loss:.4f}")
     directory = f"output/SFT-{epoch}/"
     accelerator.wait_for_everyone()
+    stats = {"perplexity": perplexity, "loss": total_loss}
+    accelerator.log(stats, step=iter_count)
     if accelerator.is_main_process:
         accelerator.unwrap_model(model).save_pretrained(directory)
         tokenizer.save_pretrained(directory)
