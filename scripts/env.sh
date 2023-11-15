f05ac0d3adc50d432a5334803c114dd5a7c56b1c
dongqian <dongqian>
2023-11-15 22:51:50 +0800

    update

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,5 +1,7 @@
 unset https_proxy
 pip install -r requirements.txt
-export https_proxy=http://172.19.57.45:3128
 git config --global user.email "dongqian"
 git config --global user.name "dongqian"
+export https_proxy=http://172.19.57.45:3128
+
+
