4827282730ef0107e8afcd268e4d61a68f8c024d
dongqian <dongqian>
2023-11-21 23:37:27 +0800

    Update at 2023-11-21 23:37:27

--- scripts/env.sh
+++ scripts/env.sh
@@ -3,9 +3,10 @@ pip install -r requirements.txt
 git config --global user.email "dongqian"
 git config --global user.name "dongqian"
 export https_proxy=http://172.19.57.45:3128
-cp -r ../data/data_of_ReGPT/WikiText-103/ ../
-cp -r ../data/data_of_ReGPT/phrases_WikiText-103/ ../
-cp -r ../data/data_of_ReGPT/llama2-7b-phrase-tokenizer-trained-on-WikiText103/ ../
-cp -r ../data/data_of_ReGPT/En-Wiki/ ../
+mkdir ../data_of_ReGPT/
+cp -r ../data/data_of_ReGPT/WikiText-103/ ../data_of_ReGPT/
+cp -r ../data/data_of_ReGPT/phrases_WikiText-103/ ../data_of_ReGPT/
+cp -r ../data/data_of_ReGPT/llama2-7b-phrase-tokenizer-trained-on-WikiText103/ ../data_of_ReGPT/
+cp -r ../data/data_of_ReGPT/En-Wiki/ ../data_of_ReGPT/
 cp -r ../data/llama2-7b/ ../
-cp -r ../data/data_of_ReGPT/c4_en/ ../
+cp -r ../data/data_of_ReGPT/c4_en/ ../data_of_ReGPT/
