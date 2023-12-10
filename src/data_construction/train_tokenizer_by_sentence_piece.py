c40ed141703a069f20c66937e29e6e7976d0fbbb
Deriq-Qian-Dong <471607065@qq.com>
2023-12-11 00:06:34 +0800

    Update at 2023-12-11 00:06:34

--- src/data_construction/train_tokenizer_by_sentence_piece.py
+++ src/data_construction/train_tokenizer_by_sentence_piece.py
@@ -4,7 +4,7 @@ import sentencepiece as spm
 corpus_name=sys.argv[1]
 start_time = time.time()
 spm.SentencePieceTrainer.train(
-    input=f'../{corpus_name}/corpus.tsv',  # 输入文件
+    input=f'../data_of_ReGPT/{corpus_name}/corpus.tsv',  # 输入文件
     model_prefix=f'llama-phrase-{corpus_name}',  # 模型前缀
     shuffle_input_sentence=True,  # 是否打乱句子
     train_extremely_large_corpus=True,
@@ -12,14 +12,14 @@ spm.SentencePieceTrainer.train(
     max_sentence_length=16384,  # 句子最大长度
     max_sentencepiece_length=128,
     model_type="BPE",
-    vocab_size=1000000,
+    vocab_size=500000,
     split_by_whitespace=False,
     split_by_number=True,
     split_digits=True,
     split_by_unicode_script=True,
     byte_fallback=True,
     allow_whitespace_only_pieces=True,
-    remove_extra_whitespaces=False,
+    remove_extra_whitespaces=True,
     normalization_rule_name="nfkc",
     num_threads=128,
 )
