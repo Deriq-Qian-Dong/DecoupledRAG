a024ea886009d3460e30367987c924a24d04c669
Deriq-Qian-Dong <471607065@qq.com>
2023-12-15 12:35:44 +0800

    Update at 2023-12-15 12:35:44

--- src/data_construction/retrieve_negatives.py
+++ src/data_construction/retrieve_negatives.py
@@ -8,7 +8,7 @@ vocab_size = 513955
 negative_depth = 101
 
 base_dir = f'../data_of_ReGPT/phrases_{corpus_name}_50k'
-embedding_path = f"{base_dir}/phrases_embeddings.npy"
+embedding_path = f"{base_dir}/phrases_embeddings_normalized.npy"
 print('load phrase embeddings from', embedding_path)
 phrase_embeddings = np.load(open(embedding_path,'rb'))
 corpus = torch.from_numpy(phrase_embeddings).half()
