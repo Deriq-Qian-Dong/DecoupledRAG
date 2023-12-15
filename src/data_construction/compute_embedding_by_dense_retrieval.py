a9780735fc47c8b4a91dafad46da28415df191ac
Deriq-Qian-Dong <471607065@qq.com>
2023-12-15 10:32:29 +0800

    Update at 2023-12-15 10:32:29

--- src/data_construction/compute_embedding_by_dense_retrieval.py
+++ src/data_construction/compute_embedding_by_dense_retrieval.py
@@ -9,15 +9,16 @@ from tqdm import tqdm
 from transformers import AutoTokenizer, AutoModel
 
 corpus_name = sys.argv[1]
-# encoder_model_name_or_path = "facebook/contriever"
-encoder_model_name_or_path = sys.argv[2]
+encoder_model_name_or_path = "../data/contriever"
+# encoder_model_name_or_path = sys.argv[2]
 
-os.makedirs(f"../data_of_ReGPT/phrases_{corpus_name}", exist_ok=True)
+base_dir = f'../data_of_ReGPT/phrases_{corpus_name}'
+os.makedirs(base_dir, exist_ok=True)
 
 phrase_embeddings = []
 batch_size = 128
 
-phrases = np.load(open(f"../data_of_ReGPT/phrases_{corpus_name}/phrases.npy",'rb'))
+phrases = np.load(open(f"{base_dir}/phrases.npy",'rb'))
 phrases = phrases.tolist()
 model = AutoModel.from_pretrained(encoder_model_name_or_path)
 model.cuda()
@@ -37,10 +38,10 @@ for i in tqdm(range(0, len(phrases), batch_size)):
     phrase_embeddings.append(embeddings.cpu().detach().numpy())
     
 phrase_embeddings = np.concatenate(phrase_embeddings)
-np.save(open(f"../data_of_ReGPT/phrases_{corpus_name}/phrases_embeddings.npy",'wb'), phrase_embeddings)
+np.save(open(f"{base_dir}/phrases_embeddings.npy",'wb'), phrase_embeddings)
 
 corpus = torch.from_numpy(phrase_embeddings)
 norms = torch.norm(corpus, p=2, dim=1)
 normalized_vectors = corpus / norms.view(-1, 1)
 normalized_vectors = normalized_vectors.numpy()
-np.save(open(f"../data_of_ReGPT/phrases_{corpus_name}/phrases_embeddings_normalized.npy",'wb'), normalized_vectors)
+np.save(open(f"{base_dir}/phrases_embeddings_normalized.npy",'wb'), normalized_vectors)
