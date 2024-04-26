d7aa0562e72312380a8b5400d4bd40badf839d3e
Deriq-Qian-Dong <471607065@qq.com>
2024-04-27 01:08:17 +0800

    Update at 2024-04-27 01:08:17

--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@ import sys
 from utils import *
 from model import LanguageModelTrainer, ReGPTLanguageModelTrainer, RAGLanguageModelTrainer
 
-
+os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
+os.environ['TORCH_USE_CUDA_DSA'] = '1'
 os.environ['http_proxy'] = 'http://gzbh-aip-paddlecloud140.gzbh:8128'
 os.environ['https_proxy'] = 'http://gzbh-aip-paddlecloud140.gzbh:8128'
 
