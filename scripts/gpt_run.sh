aa2445fc576d0358bb07cbc975b7d1fbcbb5dc68
Deriq-Qian-Dong <471607065@qq.com>
2024-04-27 01:11:54 +0800

    Update at 2024-04-27 01:11:54

--- scripts/gpt_run.sh
+++ scripts/gpt_run.sh
@@ -3,7 +3,7 @@ log_dir=${output_dir}/log
 mkdir -p ${output_dir}
 mkdir -p ${log_dir}
 sh scripts/set_gpu_num.sh
-CUDA_VISIBLE_DEVICES=0 accelerate launch --main_process_port=29500 --config_file config/default_config.yaml\
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 accelerate launch --main_process_port=29500 --config_file config/default_config.yaml\
     src/main.py config/gpt_config.yaml\
     | tee ${log_dir}/train.log  
 
