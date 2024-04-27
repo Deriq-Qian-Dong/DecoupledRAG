9c267a3bbdb4ed48305069410356ddfe82b60c6c
Deriq-Qian-Dong <471607065@qq.com>
2024-04-27 12:20:23 +0800

    Update at 2024-04-27 12:20:23

--- src/data_construction/compute_embedding_by_decoder_only_multi_gpu.py
+++ src/data_construction/compute_embedding_by_decoder_only_multi_gpu.py
@@ -17,7 +17,7 @@ def get_config(path="config/rellama_config.yaml"):
     with open(path, "r") as yaml_file:
         config = yaml.safe_load(yaml_file)
     return config
-def generating_for_decoder_only_model(config):
+def computing_for_decoder_only_model(config):
     tokenizer = AutoTokenizer.from_pretrained(config['model_name_or_path'], trust_remote_code=True)
     if tokenizer.eos_token_id is None:
         tokenizer.eos_token_id = tokenizer.eod_id
@@ -40,6 +40,7 @@ def generating_for_decoder_only_model(config):
                             padding=True,
                             truncation=True,
                             return_tensors="pt")
+        batch['pids'] = pids
         return batch
     accelerator = Accelerator()
     if accelerator.is_local_main_process:
@@ -51,31 +52,40 @@ def generating_for_decoder_only_model(config):
     data = []
     local_rank = torch.distributed.get_rank() if torch.distributed.is_initialized() else 0
     world_size = torch.distributed.get_world_size() if torch.distributed.is_initialized() else 1
-    dataset = dataset.shard(num_shards=world_size, index=local_rank, contiguous=True)
     dataloader = DataLoader(dataset, batch_size=config['batch_size'], collate_fn=collate_fn, shuffle=False)
     dataloader = accelerator.prepare_data_loader(dataloader)
     all_embeddings = []
+    all_pids = []
     with torch.no_grad():
         for batch in tqdm(dataloader, desc='computing', disable=not accelerator.is_local_main_process):
+            pids = batch.pop('pids')
             outputs = model(**batch).last_hidden_state
             embeddings = outputs[:, -1, :].detach().cpu().numpy()
             all_embeddings.append(embeddings)
+            all_pids.extend(pids)
         all_embeddings = np.concatenate(all_embeddings)
+        all_pids = np.array(all_pids)
         np.save(config['output_path']+f'_{local_rank}.npy', all_embeddings)
+        np.save(config['output_path']+f'_pids_{local_rank}.npy', all_pids)
         accelerator.wait_for_everyone()
         if accelerator.is_local_main_process:
             # merge all data
-            merged_data = []
+            merged_embeddings = []
+            merged_pids = []
             for rank in range(world_size):
                 data = np.load(config['output_path']+f'_{rank}.npy')
-                merged_data.append(data)
-            merged_data = np.concatenate(merged_data)
-            np.save(config['output_path']+".npy", merged_data)
+                merged_embeddings.append(data)
+                pids = np.load(config['output_path']+f'_pids_{rank}.npy')
+                merged_pids.extend(pids.tolist())
+            merged_embeddings = np.concatenate(merged_embeddings)
+            merged_pids = np.array(merged_pids)
+            np.save(config['output_path']+".npy", merged_embeddings)
+            np.save(config['output_path']+"_pids.npy", merged_pids)
 
 if __name__ == "__main__":
     config_path = sys.argv[1]
     config = get_config(config_path)
     if config['model_type'] == 'decoder_only':
-        generating_for_decoder_only_model(config)
+        computing_for_decoder_only_model(config)
     else:
         raise ValueError('model_type must be decoder_only or encoder_decoder')
