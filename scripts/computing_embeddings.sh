280b6de3d236843a19afdfe9f0935a133283c160
Deriq-Qian-Dong <471607065@qq.com>
2024-04-27 11:56:51 +0800

    Update at 2024-04-27 11:56:51

---
+++ scripts/computing_embeddings.sh
@@ -0,0 +1,16 @@
+output_dir=output
+log_dir=${output_dir}/log
+mkdir -p ${output_dir}
+mkdir -p ${log_dir}
+sh scripts/set_gpu_num.sh
+yaml_file=$1
+# 如果yaml_file为空，则使用默认的配置文件
+if [ ! -n "$yaml_file" ];then
+    yaml_file=config/computing_embeddings.yaml
+fi
+echo 'yaml_file: '$yaml_file
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 accelerate launch --main_process_port=29599 --config_file config/default_config.yaml\
+    src/data_construction/cimpute_embedding_by_decoder_only_multi_gpu.py ${yaml_file}\
+    | tee ${log_dir}/train.log  
+
+echo "=================done train=================="
---
+++ src/data_construction/compute_embedding_by_decoder_only_multi_gpu.py
@@ -0,0 +1,78 @@
+import os
+import sys
+import json
+import torch
+from utils import *
+from transformers import AutoTokenizer, AutoModelForCausalLM, AutoModelForSeq2SeqLM, AutoModel
+from datasets import load_dataset, load_from_disk
+from torch.utils.data import DataLoader, Dataset
+from accelerate import Accelerator
+from tqdm import tqdm
+from dataset_factory import MDQADataset
+
+os.environ['http_proxy'] = 'http://gzbh-aip-paddlecloud140.gzbh:8128'
+os.environ['https_proxy'] = 'http://gzbh-aip-paddlecloud140.gzbh:8128'
+
+
+def generating_for_decoder_only_model(config):
+    tokenizer = AutoTokenizer.from_pretrained(config['model_name_or_path'], trust_remote_code=True)
+    if tokenizer.eos_token_id is None:
+        tokenizer.eos_token_id = tokenizer.eod_id
+    try:
+        tokenizer.pad_token_id = tokenizer.eos_token_id
+    except:
+        pass
+    tokenizer.truncation_side='left'
+    tokenizer.padding_side='left'
+    model = AutoModel.from_pretrained(config['model_name_or_path'])
+    dataset = load_dataset('csv', data_files={'train': config['data_name_or_path']}, delimiter='\t',column_names=['pid', 'text'])['train']
+    def collate_fn(elems):
+        pids = []
+        texts = []
+        for elem in elems:
+            pids.append(elem['pid'])
+            texts.append(elem['text'])
+        batch = tokenizer(texts,
+                            max_length=config['max_length'],
+                            padding=True,
+                            truncation=True,
+                            return_tensors="pt")
+        return batch
+    accelerator = Accelerator()
+    if accelerator.is_local_main_process:
+        print('start computing...')
+        for key in config:
+            print(f'{key}: {config[key]}')
+    model = accelerator.prepare_model(model)
+    model.eval()
+    data = []
+    local_rank = torch.distributed.get_rank() if torch.distributed.is_initialized() else 0
+    world_size = torch.distributed.get_world_size() if torch.distributed.is_initialized() else 1
+    dataset = dataset.shard(num_shards=world_size, index=local_rank, contiguous=True)
+    dataloader = DataLoader(dataset, batch_size=config['batch_size'], collate_fn=collate_fn, shuffle=False)
+    dataloader = accelerator.prepare_data_loader(dataloader)
+    all_embeddings = []
+    with torch.no_grad():
+        for batch in tqdm(dataloader, desc='computing', disable=not accelerator.is_local_main_process):
+            outputs = model(**batch).last_hidden_state
+            embeddings = outputs[:, -1, :].detach().cpu().numpy()
+            all_embeddings.append(embeddings)
+        all_embeddings = np.concatenate(all_embeddings)
+        np.save(config['output_path']+f'_{local_rank}.npy', all_embeddings)
+        accelerator.wait_for_everyone()
+        if accelerator.is_local_main_process:
+            # merge all data
+            merged_data = []
+            for rank in range(world_size):
+                data = np.load(config['output_path']+f'_{rank}.npy')
+                merged_data.append(data)
+            merged_data = np.concatenate(merged_data)
+            np.save(config['output_path']+".npy", merged_data)
+
+if __name__ == "__main__":
+    config_path = sys.argv[1]
+    config = get_config(config_path)
+    if config['model_type'] == 'decoder_only':
+        generating_for_decoder_only_model(config)
+    else:
+        raise ValueError('model_type must be decoder_only or encoder_decoder')
