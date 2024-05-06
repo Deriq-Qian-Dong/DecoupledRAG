3c0fed3dfefa4b82d5563d32dab65c9b50008194
dongqian06 <dongqian06@MacBook-Pro.local>
2024-05-06 19:30:15 +0800

    Update at 2024-05-06 19:30:15

--- src/model.py
+++ src/model.py
@@ -398,6 +398,7 @@ class RAGLanguageModelTrainer(LanguageModelTrainer):
     
     def task_specific_stats(self, stats, model):
         stats['retrieval_position'] = self.accelerator.unwrap_model(model).model.base_model.config.retrieval_position
-        stats['gate_score'] = self.accelerator.unwrap_model(model).model.base_model.layers[-1].gate_crossattention.cpu().detach().float().numpy()
+        for i in range(self.config['training']['add_cross_attention_layer_number']):
+            stats[f'gate_score_{i}'] = self.accelerator.unwrap_model(model).model.base_model.layers[-i-1].gate_crossattention.cpu().detach().float().numpy()
         return stats
 
