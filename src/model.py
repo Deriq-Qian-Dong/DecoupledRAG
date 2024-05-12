fd5e45c1158c1e98db9db329bef390c03f93d75b
Deriq-Qian-Dong <471607065@qq.com>
2024-05-13 00:28:22 +0800

    Update at 2024-05-13 00:28:22

--- src/model.py
+++ src/model.py
@@ -195,7 +195,7 @@ class LanguageModelTrainer:
         self.best_perplexity = 1e10
 
     def run(self):
-        self.test()
+        # self.test()
         for epoch in range(self.train_config['start_from'], self.train_config['num_epochs']):
             self.epoch = epoch
             self.set_epoch_to_dataset()
