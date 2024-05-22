e9555ba0b449fefdd660fe0cc4963963eb74411f
dongqian06 <dongqian06@MacBook-Pro.local>
2024-05-22 12:15:30 +0800

    Update at 2024-05-22 12:15:30

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,17 +1,23 @@
 unset https_proxy
-pip install -r requirements.txt
+# pip install -r requirements.txt
+cp ../data/packs_torch.tar ./
+tar xvf packs_torch.tar
+pip install --no-index --find-links=./packs_torch -r requirements.txt
 git config --global user.email "dongqian"
 git config --global user.name "dongqian"
 export https_proxy='http://agent.baidu.com:8891'
 export http_proxy='http://agent.baidu.com:8891'
 mkdir -p ../data_of_ReGPT
 mkdir -p ../data_of_ReGPT/marco
-cp -r ../data/data_of_ReGPT/marco/sorted_datasets_train_llama2/ ../data_of_ReGPT/marco
-cp -r ../data/data_of_ReGPT/marco/sorted_datasets_test_llama2/ ../data_of_ReGPT/marco
+# cp -r ../data/data_of_ReGPT/marco/sorted_datasets_train_llama2/ ../data_of_ReGPT/marco
+# cp -r ../data/data_of_ReGPT/marco/sorted_datasets_test_llama2/ ../data_of_ReGPT/marco
 cp -r ../data/data_of_ReGPT/marco/phrases_embeddings.npy ../data_of_ReGPT/marco
+cp -r ../data/data_of_ReGPT/marco/collection/ ../data_of_ReGPT/marco
 # cp -r ../data/data_of_ReGPT/WikiText-103/ ../data_of_ReGPT/
 # cp -r ../data/data_of_ReGPT/phrases_WikiText-103/ ../data_of_ReGPT/
 # cp -r ../data/data_of_ReGPT/llama2-7b-phrase-tokenizer-trained-on-WikiText103/ ../data_of_ReGPT/
 # cp -r ../data/data_of_ReGPT/En-Wiki/ ../data_of_ReGPT/
 # cp -r ../data/llama2-7b/ ../
 # cp -r ../data/data_of_ReGPT/c4_en/ ../data_of_ReGPT/
+sh scripts/update_transformers.sh
+cp -r ../data/rag_llama2/24_ca_layer/ ../
--- src/test.py
+++ src/test.py
@@ -1,48 +1,30 @@
-import os
-import sys
-import torch
-import random
-import datetime
-import numpy as np
-from utils import *
-from tqdm import tqdm
-from time import time
-from torch import Tensor, nn
-from dataset_factory import *
-import torch_optimizer as optim
-import torch.distributed as dist
-from dataclasses import dataclass
-from accelerate import Accelerator
-from transformers.file_utils import ModelOutput
-from torch.utils.data import DataLoader, Dataset
-from typing import List, Optional, Tuple, Union, Dict
-from deepspeed.ops.adam import DeepSpeedCPUAdam, FusedAdam
-from torch.optim.lr_scheduler import CosineAnnealingLR, LinearLR
-from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel, AutoConfig
-try:
-    from transformers import GPT2LMandRetrievalHeadsModel, LlamaWithRetrievalHeadForCausalLM
-except:
-    GPT2LMandRetrievalHeadsModel, LlamaWithRetrievalHeadForCausalLM = None, None
-from peft import LoraConfig, get_peft_model, PeftModel, TaskType
+from transformers import AutoConfig, LlamaWithRetrievalHeadForInference, AutoTokenizer
+model_path = '../24_ca_layer/SFT-best/'
+config = AutoConfig.from_pretrained(model_path)
+config.negatives_x_device = True
 
-tokenizer = AutoTokenizer.from_pretrained('./output/SFT-new/')
-args = {'data_name_or_path':'../data_of_ReGPT/marco/sorted_datasets_train_llama2/','max_seq_len':256}
-data = RAGPretrainDataset(tokenizer, args)
-max_tokens = 16384  # 设置每个batch的最大token数
-accelerator = Accelerator(log_with='tensorboard', project_dir='output_test/')
-print('world size: %d\nlocal rank: %d' % (accelerator.num_processes, accelerator.process_index))
-sampler = DynamicBatchSampler(data, max_tokens, num_replicas=accelerator.num_processes, rank=accelerator.process_index)
-dataloader = DataLoader(data, batch_sampler=sampler, shuffle=False, collate_fn=data._collate_fn)
-print('dataloader length: %d\nlocal rank: %d' % (len(dataloader), accelerator.process_index))
-current_time = datetime.datetime.now()
-timestamp = current_time.strftime("%Y-%m-%d-%H-%M-%S")
-accelerator.init_trackers(project_name=f'test_{timestamp}')
-# dataloader = accelerator.prepare_data_loader(dataloader)
-pbar = tqdm(total=len(dataloader))
-for step, batch in enumerate(dataloader):
-    batch_size = batch.input_ids.shape[0]
-    stats = {'batch_size': batch_size}
-    accelerator.log(stats, step)
-    if accelerator.is_main_process:
-        pbar.update(1)
-print(step, accelerator.process_index)
+config.kb_path = '../data_of_ReGPT/marco/phrases_embeddings.npy'
+config.retrieval_step = 10
+config.topk = 6
+config.q_reps_cache_type = 'QRepsCache'
+config.q_reps_cache_window_size = 10
+
+model = LlamaWithRetrievalHeadForInference.from_pretrained(model_path, config=config)          
+model = model.cuda()
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+
+input_text = 'The presence of communication amid scientific minds was equally important to the success of the Manhattan Project as scientific intellect was. The only cloud hanging over the impressive achievement of the atomic researchers and engineers is what their success truly meant; hundreds of thousands of innocent lives obliterated.'
+input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to("cuda")
+
+input_ids = input_ids[:,:30]
+outputs = model.generate(input_ids,max_new_tokens=100)
+print(tokenizer.decode(outputs[0]))
+from datasets import load_dataset, load_from_disk
+dataset = load_from_disk('../data_of_ReGPT/marco/collection/')
+while True:
+    input_text = 'The presence of communication amid scientific minds was equally important to the success of the Manhattan Project as scientific intellect was. The only cloud hanging over the impressive achievement of the atomic researchers and engineers is what their success truly meant; hundreds of thousands of innocent lives obliterated.'
+    input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to("cuda")
+    
+    input_ids = input_ids[:,:30]
+    outputs = model.generate(input_ids,max_new_tokens=100)
