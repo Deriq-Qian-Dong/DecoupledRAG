dc802ca57069420622338e64fa00ecac99e8c0c1
Deriq-Qian-Dong <471607065@qq.com>
2024-05-22 14:34:54 +0800

    Update at 2024-05-22 14:34:54

--- transformers/src/transformers/models/llama/modeling_llama.py
+++ transformers/src/transformers/models/llama/modeling_llama.py
@@ -1758,7 +1758,7 @@ class LlamaWithRetrievalHeadForInference(LlamaPreTrainedModel):
         >>> tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
         "Hey, are you conscious? Can you talk to me?\nI'm not conscious, but I can talk to you."
         ```"""
-        print(encoder_hidden_states)
+        print(encoder_hidden_states, encoder_hidden_states.size())
         output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
         output_hidden_states = (
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
