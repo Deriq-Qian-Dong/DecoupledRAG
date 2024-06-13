dbdf8008defe0b9c2e233aa7301e38d9e40cf53d
Deriq-Qian-Dong <471607065@qq.com>
2024-06-13 14:58:16 +0800

    Update at 2024-06-13 14:58:16

--- src/main.py
+++ src/main.py
@@ -5,8 +5,8 @@ from model import LanguageModelTrainer, ReGPTLanguageModelTrainer, RAGLanguageMo
 
 os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
 os.environ['TORCH_USE_CUDA_DSA'] = '1'
-os.environ['http_proxy'] = 'http://gzbh-aip-paddlecloud140.gzbh:8128'
-os.environ['https_proxy'] = 'http://gzbh-aip-paddlecloud140.gzbh:8128'
+os.environ['http_proxy'] = 'http://agent.baidu.com:8891'
+os.environ['https_proxy'] = 'http://agent.baidu.com:8891'
 
 TrainerClass = {'LanguageModelTrainer': LanguageModelTrainer, 'ReGPTLanguageModelTrainer': ReGPTLanguageModelTrainer, "RAGLanguageModelTrainer":RAGLanguageModelTrainer, "RAGLanguageModelTester":RAGLanguageModelTester, "RAGQATester":RAGQATester}
 
