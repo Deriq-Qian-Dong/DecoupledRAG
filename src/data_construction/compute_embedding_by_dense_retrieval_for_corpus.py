557a9d03c861c7184881ac76ac60fa6334b16a5b
Deriq-Qian-Dong <471607065@qq.com>
2024-06-14 22:21:32 +0800

    Update at 2024-06-14 22:21:32

--- src/data_construction/compute_embedding_by_dense_retrieval_for_corpus.py
+++ src/data_construction/compute_embedding_by_dense_retrieval_for_corpus.py
@@ -7,38 +7,57 @@ import torch
 import numpy as np
 from tqdm import tqdm
 from transformers import AutoTokenizer, AutoModel
+from datasets import load_from_disk
 
 encoder_model_name_or_path = "../data/RetroMAE_MSMARCO_distill/"
 # encoder_model_name_or_path = sys.argv[2]
 
-base_dir = f'../data_of_ReGPT/marco'
+base_dir = f'../data_of_ReGPT/En-Wiki'
 os.makedirs(base_dir, exist_ok=True)
 
 phrase_embeddings = []
 batch_size = 2048
 
 phrases = []
-with open("../data_of_ReGPT/marco/collection.tsv") as f:
-    lines = f.readlines()
-for line in lines:
-    phrases.append(line.split("\t")[1].strip())
+data = load_from_disk("../data_of_ReGPT/En-Wiki/sorted_datasets_train")
+torch.distributed.init_process_group(backend="nccl", init_method='env://')
+local_rank = torch.distributed.get_rank()
+world_size = torch.distributed.get_world_size()
+data = data.shard(num_shards=world_size, index=local_rank, contiguous=True)
+for item in data:
+    phrases.extend(item['text'])
+# with open("../data_of_ReGPT/marco/collection.tsv") as f:
+    # lines = f.readlines()
+# for line in lines:
+    # phrases.append(line.split("\t")[1].strip())
 print('load model from', encoder_model_name_or_path)
+torch.cuda.set_device(local_rank)
+device = torch.device("cuda", local_rank)
 model = AutoModel.from_pretrained(encoder_model_name_or_path)
-model.cuda()
+model.to(device)
 model.eval()
 tokenizer = AutoTokenizer.from_pretrained(encoder_model_name_or_path)
 
 for i in tqdm(range(0, len(phrases), batch_size)):
     texts = phrases[i:i+batch_size]
     inputs = tokenizer(texts, padding=True, truncation=True, return_tensors='pt', max_length=128)
-    inputs.to("cuda")
+    inputs.to(device)
     with torch.no_grad():
         outputs = model(**inputs)
     embeddings = outputs.last_hidden_state[:,0,:]
     phrase_embeddings.append(embeddings.cpu().detach().numpy())
     
 phrase_embeddings = np.concatenate(phrase_embeddings)
-np.save(open(f"{base_dir}/phrases_embeddings.npy",'wb'), phrase_embeddings)
+np.save(open(f"{base_dir}/phrases_embeddings_{local_rank}.npy",'wb'), phrase_embeddings)
+# wait for all processes to finish
+torch.distributed.barrier()
+if local_rank == 0:
+    phrase_embeddings = []
+    for i in range(world_size):
+        phrase_embeddings.append(np.load(open(f"{base_dir}/phrases_embeddings_{i}.npy",'rb')))
+    phrase_embeddings = np.concatenate(phrase_embeddings)
+    np.save(open(f"{base_dir}/phrases_embeddings.npy",'wb'), phrase_embeddings)
+    print('save phrase embeddings to', f"{base_dir}/phrases_embeddings.npy")
 
 # corpus = torch.from_numpy(phrase_embeddings)
 # norms = torch.norm(corpus, p=2, dim=1)
