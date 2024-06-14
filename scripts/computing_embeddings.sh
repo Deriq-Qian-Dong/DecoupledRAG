4bcb4eb1015184bfc005bc30c47cdcd0c681c996
Deriq-Qian-Dong <471607065@qq.com>
2024-06-14 22:35:01 +0800

    Update at 2024-06-14 22:35:01

--- scripts/computing_embeddings.sh
+++ scripts/computing_embeddings.sh
@@ -9,8 +9,11 @@ if [ ! -n "$yaml_file" ];then
     yaml_file=config/computing_embeddings.yaml
 fi
 echo 'yaml_file: '$yaml_file
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 accelerate launch --main_process_port=29599 --config_file config/default_config.yaml\
+#     src/data_construction/compute_embedding_by_decoder_only_multi_gpu.py ${yaml_file}\
+#     | tee ${log_dir}/train.log  
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 accelerate launch --main_process_port=29599 --config_file config/default_config.yaml\
-    src/data_construction/compute_embedding_by_decoder_only_multi_gpu.py ${yaml_file}\
+    src/data_construction/compute_embedding_by_dense_retrieval_for_corpus.py \
     | tee ${log_dir}/train.log  
 
 echo "=================done train=================="
