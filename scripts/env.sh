4e6a4145c25de014f17abcd957291c43f0e3c086
Deriq-Qian-Dong <471607065@qq.com>
2024-06-16 23:39:26 +0800

    Update at 2024-06-16 23:39:26

--- scripts/env.sh
+++ scripts/env.sh
@@ -8,19 +8,20 @@ git config --global user.name "dongqian"
 export https_proxy='http://agent.baidu.com:8891'
 export http_proxy='http://agent.baidu.com:8891'
 mkdir -p ../data_of_ReGPT
-mkdir -p ../data_of_ReGPT/marco
+mkdir -p ../data_of_ReGPT/En-Wiki
 # cp -r ../data/data_of_ReGPT/marco/sorted_datasets_train_llama2/ ../data_of_ReGPT/marco
-cp -r ../data/data_of_ReGPT/marco/sorted_datasets_test_llama2/ ../data_of_ReGPT/marco
-cp -r ../data/data_of_ReGPT/marco/phrases_embeddings.npy ../data_of_ReGPT/marco
-cp -r ../data/data_of_ReGPT/marco/collection/ ../data_of_ReGPT/marco
-cp -r ../data/data_of_ReGPT/msmarco_qa/ ../data_of_ReGPT/
+# cp -r ../data/data_of_ReGPT/marco/sorted_datasets_test_llama2/ ../data_of_ReGPT/marco
+# cp -r ../data/data_of_ReGPT/marco/phrases_embeddings.npy ../data_of_ReGPT/marco
+# cp -r ../data/data_of_ReGPT/marco/collection/ ../data_of_ReGPT/marco
+# cp -r ../data/data_of_ReGPT/msmarco_qa/ ../data_of_ReGPT/
 # cp -r ../data/data_of_ReGPT/WikiText-103/ ../data_of_ReGPT/
 # cp -r ../data/data_of_ReGPT/phrases_WikiText-103/ ../data_of_ReGPT/
 # cp -r ../data/data_of_ReGPT/llama2-7b-phrase-tokenizer-trained-on-WikiText103/ ../data_of_ReGPT/
 # cp -r ../data/data_of_ReGPT/En-Wiki/ ../data_of_ReGPT/
 # cp -r ../data/llama2-7b/ ../
 # cp -r ../data/data_of_ReGPT/c4_en/ ../data_of_ReGPT/
+cp -r ../data/data_of_ReGPT/En-Wiki/sorted_datasets_test/ ../data_of_ReGPT/En-Wiki/
 sh scripts/update_transformers.sh
-cp -r ../data/rag_llama2/24_ca_layer/ ../
+cp -r ../data/rag_llama2/24_wiki/ ../
 mkdir -p output
 mv ../24_ca_layer/* output/
