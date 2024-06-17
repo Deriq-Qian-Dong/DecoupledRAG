7db29d25ad5547dbeff4f68d9541af1b2e264d96
Deriq-Qian-Dong <471607065@qq.com>
2024-06-17 14:18:16 +0800

    Update at 2024-06-17 14:18:16

--- src/model.py
+++ src/model.py
@@ -715,7 +715,7 @@ class RAGQAWoTFTester(RAGQATester):
         super().__init__(config)
 
     def run(self):
-        for i in range(10, 50):
+        for i in range(2, 50):
             self.config['generation_kwargs']['max_new_tokens'] = i
             rouge1 = self.run_wo_teacher_forcing(1000000)
             stats = {}
