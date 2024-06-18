d85b6f59165d505a5607a3fe9541b48d98a1de40
Deriq-Qian-Dong <471607065@qq.com>
2024-06-18 18:09:53 +0800

    Update at 2024-06-18 18:09:53

--- src/prompt_templates.py
+++ src/prompt_templates.py
@@ -1,23 +1,23 @@
 QA_PROMPT = """Answer the question.
-                       Answer the question in ONE OR FEW WORDS
-                       Here are some examples:
-                        #
-                        Question: Who is Catherine Of Pomerania, Countess Palatine Of Neumarkt’s father-in-law?
-                        Expected Output:
-                        Rupert III of the Palatinate
-                        #
-                        Question: Which film has the director died first, Crimen A Las Tres or The Working Class Goes To Heaven?
-                        Expected Output:
-                        The Working Class Goes to Heaven
-                        #
-                        Question: Which genus of moth in the world's seventh-largest country contains only one species?
-                        Expected Output:
-                        Crambidae
-                        #
-                        Question: What is the length of the track where the 2013 Liqui Moly Bathurst 12 Hour was staged?
-                        Expected Output:
-                        6.213 km long
-                        #
-                        Question: {question}
-                        Expected Output:
+Answer the question in ONE OR FEW WORDS
+Here are some examples:
+#
+Question: Who is Catherine Of Pomerania, Countess Palatine Of Neumarkt’s father-in-law?
+Expected Output:
+Rupert III of the Palatinate
+#
+Question: Which film has the director died first, Crimen A Las Tres or The Working Class Goes To Heaven?
+Expected Output:
+The Working Class Goes to Heaven
+#
+Question: Which genus of moth in the world's seventh-largest country contains only one species?
+Expected Output:
+Crambidae
+#
+Question: What is the length of the track where the 2013 Liqui Moly Bathurst 12 Hour was staged?
+Expected Output:
+6.213 km long
+#
+Question: {question}
+Expected Output:
 """
