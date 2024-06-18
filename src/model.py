cec2be87f579de7b5611221519454a06ae759634
Deriq-Qian-Dong <471607065@qq.com>
2024-06-18 18:23:28 +0800

    Update at 2024-06-18 18:23:28

--- src/model.py
+++ src/model.py
@@ -635,7 +635,7 @@ class RAGQATester(RAGLanguageModelTester):
         with torch.no_grad():
             for step, batch in enumerate(test_dataloader):
                 answers = batch.pop('answers')
-                outputs = model.generate(**batch, **generation_kwargs, stop_strings=["#","\n"], tokenizer=tokenizer)
+                outputs = model.generate(**batch, **generation_kwargs, stop_strings=["#"], tokenizer=tokenizer)
                 responses = tokenizer.batch_decode(outputs, skip_special_tokens=True)
                 answers = tokenizer.batch_decode(answers, skip_special_tokens=True)
                 input_ids = batch['input_ids']
