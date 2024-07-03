6becf63eda5783dd23545f1ae07f79038666d129
Deriq-Qian-Dong <471607065@qq.com>
2024-07-03 15:26:41 +0800

    Update at 2024-07-03 15:26:41

--- scripts/env.sh
+++ scripts/env.sh
@@ -21,7 +21,6 @@ mkdir -p ../data_of_ReGPT/En-Wiki
 # cp -r ../data/llama2-7b/ ../
 # cp -r ../data/data_of_ReGPT/c4_en/ ../data_of_ReGPT/
 cp -r ../data/data_of_ReGPT/En-Wiki/sorted_datasets_test/ ../data_of_ReGPT/En-Wiki/
-cp -r ../data/data_of_ReGPT/hotpotqaAnd2WikiMultihopQA/ ../data_of_ReGPT/
 sh scripts/update_transformers.sh
 cp -r ../data/rag_llama2/24_qa/ ../
 mkdir -p output
@@ -29,3 +28,4 @@ mv ../24_qa/* output/
 cp -r ../data/rag_llama2/24_wiki/ ../
 mkdir -p ../data_of_ReGPT/Wiki-corpus
 cp -r ../data/data_of_ReGPT/Wiki-corpus/phrases_embeddings.npy ../data_of_ReGPT/Wiki-corpus/
+cp -r ../data/data_of_ReGPT/QA_datasets ../data_of_ReGPT/
