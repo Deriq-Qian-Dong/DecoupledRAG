08a26531f4ecbdca1a717201410f6d21f95cb733
qian <471607065@qq.com>
2024-08-15 16:55:09 +0800

    Update at 2024-08-15 16:55:09

--- src/model.py
+++ src/model.py
@@ -458,7 +458,7 @@ class RAGLanguageModelTrainer(LanguageModelTrainer):
     
     def task_specific_stats(self, stats, model):
         for i in range(self.config['training']['add_cross_attention_layer_number']):
-            stats[f'gate_score/{i}'] = float(self.accelerator.unwrap_model(model).model.base_model.layers[-i-1].gate_crossattention.cpu().detach().float().numpy()[0])
+            stats[f'gate_score/{i}'] = float(self.accelerator.unwrap_model(model).model.base_model.layers[i].gate_crossattention.cpu().detach().float().numpy()[0])
         return stats
 
 @register_class
