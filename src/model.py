2d6c5db50a1e15472772fd46c157a7e1da026545
qian <471607065@qq.com>
2024-08-15 22:56:04 +0800

    Update at 2024-08-15 22:56:04

--- src/model.py
+++ src/model.py
@@ -202,7 +202,7 @@ class RAGForCausalLM(nn.Module):
         return outputs
     
     def save_pretrained(self, directory):
-        self.model.save_pretrained(directory) 
+        self.model.model.save_pretrained(directory) 
         gate_scores = []
         for i in range(self.train_config['add_cross_attention_layer_number']+1):
             gate_scores.append(float(self.model.model.layers[i].gate_crossattention.cpu().detach().float().numpy()[0]))
