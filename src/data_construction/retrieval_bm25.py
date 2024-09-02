6d06fdb635ffe7e7c7f8b6fe82946812631a5c92
Deriq-Qian-Dong <471607065@qq.com>
2024-09-02 21:06:30 +0800

    Update at 2024-09-02 21:06:30

---
+++ src/data_construction/retrieval_bm25.py
@@ -0,0 +1,16 @@
+from pyserini.search.lucene import LuceneSearcher
+from datasets import load_from_disk
+from tqdm import tqdm
+searcher = LuceneSearcher('../data_of_ReGPT/Wiki-corpus/bm25_index/')
+searcher.set_bm25(0.82, 0.68)
+
+data = load_from_disk('../data_of_ReGPT/QA_datasets/nq/sorted_datasets_test')
+with open('./output/res.top5.nq.test', 'w') as f:
+    for example in tqdm(data):
+        query = example['question']
+        hits = searcher.search(query, 5)
+        for hit in hits:
+            f.write(f"{example['id']}\t{hit.docid}\t{hit.score}\t{hit.text}\n")
+
+    
+
