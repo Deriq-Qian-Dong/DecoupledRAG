fa9acb01fb5b68e532b03bd7119f42af6237deea
qian <471607065@qq.com>
2024-09-03 10:14:17 +0800

    Update at 2024-09-03 10:14:17

--- scripts/rag_llama_qa_run.sh
+++ scripts/rag_llama_qa_run.sh
@@ -4,7 +4,7 @@ log_dir=${output_dir}/log
 mkdir -p ${output_dir}
 mkdir -p ${log_dir}
 sh scripts/set_gpu_num.sh
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 accelerate launch --main_process_port=29500 --config_file config/default_config.yaml\
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 accelerate launch --main_process_port=29501 --config_file config/default_config.yaml\
     src/main.py config/rag_llama_qaconfig.yaml\
     | tee ${log_dir}/train.log  
 
