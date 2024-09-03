b6447213a0b7313cfb3d5277c3441321ab2ab5c0
qian <471607065@qq.com>
2024-09-03 18:45:41 +0800

    Update at 2024-09-03 18:45:41

--- src/dataset_factory.py
+++ src/dataset_factory.py
@@ -411,10 +411,10 @@ class QADataset4Chat(Dataset):
             answer = sample['answer']
         # hits = self.searcher.search(query, 5)
         retrieved_docs = self.corpus[sample['neighbors']]['text'][:1]
-        references = "references:\n"
-        for doc in retrieved_docs:
-            references += doc+'\n'
-        query = references+query+'\nThe answer MUST in ONE OR FEW WORDS.'
+        # references = "references:\n"
+        # for doc in retrieved_docs:
+            # references += doc+'\n'
+        query = query+'\nThe answer MUST in ONE OR FEW WORDS.'
         chat = [{'role': 'user', 'content': query}, {'role': 'assistant', 'content': answer}]
         chat = self.tokenizer.apply_chat_template(chat, tokenize=False, add_generation_prompt=False)
         neighbor_embeddings = sample.get('neighbor_embeddings')
@@ -479,10 +479,10 @@ class QADataset4ChatTest(QADataset4Chat):
             answer = sample['answer']
         # hits = self.searcher.search(query, 5)
         retrieved_docs = self.corpus[sample['neighbors']]['text'][:1]
-        references = "references:\n"
-        for doc in retrieved_docs:
-            references += doc+'\n'
-        query = references+query+'\nThe answer MUST in ONE OR FEW WORDS.'
+        # references = "references:\n"
+        # for doc in retrieved_docs:
+            # references += doc+'\n'
+        query = query+'\nThe answer MUST in ONE OR FEW WORDS.'
         chat = [{'role': 'user', 'content': query}]
         chat = self.tokenizer.apply_chat_template(chat, tokenize=False, add_generation_prompt=True)
         neighbor_embeddings = sample.get('neighbor_embeddings')
