3abfae896b6518a7e4e122227d177e13618bb162
qian <471607065@qq.com>
2024-09-10 01:11:54 +0800

    Update at 2024-09-10 01:11:54

--- src/contrastive_generation_vllm.py
+++ src/contrastive_generation_vllm.py
@@ -32,6 +32,30 @@ class QADataset4Chat(Dataset):
 
     def get_batch(self, indices):
         return [self.__getitem__(idx) for idx in indices]
+    
+class QADataset4ChatWithBackgroundKnowledge(QADataset4Chat):
+    def __init__(self, tokenizer, args):
+        super().__init__(tokenizer, args)
+
+    def __getitem__(self, idx):
+        sample = self.datasets[idx]
+        tokenizer = self.tokenizer
+        query = tokenizer.decode(tokenizer.encode(sample['prompt'], add_special_tokens=False), skip_special_tokens=True).replace('user\n\n', '').replace('assistant\n\n', '')
+        answers = sample['answers']
+        chats = []
+        for answer in answers:
+            chat = [{'role': 'user', 'content': query}, {'role': 'assistant', 'content': answer}]
+            chat.append({'role': 'user', 'content': 'Please provide the background knowledge for the answer. The background knowledge MUST be within 256 tokens.'})
+            chat = tokenizer.apply_chat_template(chat, tokenize=False, add_generation_prompt=True)
+            chats.append(chat)
+        return chats
+    
+    def get_batch(self, indices):
+        batchs = []
+        for idx in indices:
+            batchs.extend(self.__getitem__(idx))
+        return batchs
+
 
 def initialize_tokenizer(model_path):
     return AutoTokenizer.from_pretrained(model_path)
@@ -59,7 +83,7 @@ def save_dataset(data, path):
     dataset = HFDataset.from_list(data)
     dataset.save_to_disk(path)
 
-def main(data_name_or_path, output_path, llm, model_name_or_path):
+def _generate_contrastive_answers(data_name_or_path, output_path, llm, model_name_or_path):
     dataset_config = {
         "number_of_docs": 1,
         "data_name_or_path": data_name_or_path,
@@ -74,13 +98,41 @@ def main(data_name_or_path, output_path, llm, model_name_or_path):
     
     save_dataset(new_data, output_path)
 
-if __name__ == "__main__":
-    candidates = ['hotpotqa/', 'nq/', 'openbookqa/', 'truthful_qa/']
+def generate_contrastive_answers():
+    candidates = ['hotpotqa/', 'nq/']
     model_name_or_path = "../llama3-chat"
     llm = initialize_llm(model_name_or_path)
     for candidate in candidates:
         print(f"Processing {candidate}...")
         data_path = f"../data_of_ReGPT/QA_datasets_woEmb/{candidate}/sorted_datasets_train"
         output_path = data_path.replace("QA_datasets_woEmb", "QA_datasets_contrastive")
-        main(data_path, output_path, llm, model_name_or_path)
+        _generate_contrastive_answers(data_path, output_path, llm, model_name_or_path)
 
+def _generate_background_knowledge_for_answers(data_name_or_path, output_path, llm, model_name_or_path):
+    dataset_config = {
+        "number_of_docs": 1,
+        "data_name_or_path": data_name_or_path,
+        "corpus": '../data_of_ReGPT/Wiki-corpus/train',
+        "max_seq_len": 512,
+    }
+    tokenizer = initialize_tokenizer(model_name_or_path)
+    datasets = QADataset4ChatWithBackgroundKnowledge(tokenizer, dataset_config)
+    batch_size = 4096    
+    sampling_params = SamplingParams(temperature=1.0, top_p=1.0, top_k=100, n=4, max_tokens=256)
+    new_data = process_batches(datasets, llm, batch_size, sampling_params)
+    
+    save_dataset(new_data, output_path)
+
+def generate_background_knowledge_for_answers():
+    model_name_or_path = "../llama3-chat"
+    llm = initialize_llm(model_name_or_path)
+    candidates = ['nq/', '2WikiMultihopQA', 'hotpotqa/', 'msmarco_qa/']
+    for candidate in candidates:
+        print(f"Processing {candidate}...")
+        data_path = f"../data_of_ReGPT/QA_datasets_contrastive/{candidate}/sorted_datasets_train"
+        output_path = data_path.replace("QA_datasets_contrastive", "QA_datasets_contrastive_with_background_knowledge")
+        _generate_background_knowledge_for_answers(data_path, output_path, llm, model_name_or_path)
+
+if __name__ == "__main__":
+    # generate_contrastive_answers()
+    generate_background_knowledge_for_answers()
