009d5f19435196cd2c93bc74a8cdfac049097efc
qian <471607065@qq.com>
2024-09-12 23:28:45 +0800

    Update at 2024-09-12 23:28:45

---
+++ scripts/srun.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# srun --nodelist=fu5l6puknblo9-0 --time=00:10:00 --pty /bin/bash
+# 提示用户输入节点编号
+echo "请输入节点编号 (1-3):"
+read n
+
+# 节点列表
+nodes=("1pdijpn14trqa-0" "d4ofqnu7t5ab9-0" "fu5l6puknblo9-0")
+
+# 检查输入是否有效
+if [[ $n -lt 1 || $n -gt ${#nodes[@]} ]]; then
+  echo "无效的节点编号。请输入一个介于 1 和 ${#nodes[@]} 之间的数字。"
+  exit 1
+fi
+
+# 获取对应的节点
+node=${nodes[$((n-1))]}
+srun --nodelist=$node --time=00:10:00 --pty /bin/bash
