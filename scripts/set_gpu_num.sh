c3361ff440cb236daa01e3d10b8f6bf9fb55af18
qian <471607065@qq.com>
2024-09-13 00:09:16 +0800

    Update at 2024-09-13 00:09:16

--- scripts/set_gpu_num.sh
+++ scripts/set_gpu_num.sh
@@ -1,6 +1,7 @@
 cat config/default_config.yaml.sample > config/default_config.yaml
-num_processes=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
-echo "num_processes: "$num_processes >>  config/default_config.yaml
+# num_processes=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
+# echo "num_processes: "$num_processes >>  config/default_config.yaml
 # 获取 CUDA_VISIBLE_DEVICES 环境变量
 cuda_devices=${CUDA_VISIBLE_DEVICES:-"Not Set"}
-echo "CUDA_VISIBLE_DEVICES: $cuda_devices" >> config/default_config.yaml
+num_processes=$(echo "$cuda_devices" | awk -F',' '{print NF}')
+echo "num_processes: $num_processes" >> config/default_config.yaml
