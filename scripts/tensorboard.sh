049c250d1a19ee547fd9197364b5705227b09b2f
qian <471607065@qq.com>
2024-09-13 01:40:22 +0800

    Update at 2024-09-13 01:40:22

--- scripts/tensorboard.sh
+++ scripts/tensorboard.sh
@@ -1,2 +1,2 @@
-nohup tensorboard --logdir ./output --host $(hostname -i) --port 8040 > tensorboard.log 2>&1 &
+nohup tensorboard --logdir ./output --port 8040 > tensorboard.log 2>&1 &
 # nohup tensorboard --logdir ./output --host=localhost --port 8040 > tensorboard.log 2>&1 &
