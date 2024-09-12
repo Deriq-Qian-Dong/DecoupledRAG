c865da13cad9bc1ad1f754a6942fd94c3b140d15
qian <471607065@qq.com>
2024-09-13 00:04:11 +0800

    Update at 2024-09-13 00:04:11

--- scripts/retrieval.sh
+++ scripts/retrieval.sh
@@ -2,6 +2,7 @@ output_dir=output
 log_dir=${output_dir}/log
 mkdir -p ${output_dir}
 mkdir -p ${log_dir}
+export CUDA_VISIBLE_DEVICES=0,4,5,6
 sh scripts/set_gpu_num.sh
 yaml_file=$1
 # 如果yaml_file为空，则使用默认的配置文件
@@ -9,7 +10,7 @@ if [ ! -n "$yaml_file" ];then
     yaml_file=config/retrieval.yaml
 fi
 echo 'yaml_file: '$yaml_file
-CUDA_VISIBLE_DEVICES=0,4,5,6 accelerate launch --main_process_port=29511 --config_file config/default_config.yaml\
+accelerate launch --main_process_port=29511 --config_file config/default_config.yaml\
     src/retrieval.py ${yaml_file}\
     | tee ${log_dir}/train.log  
 
