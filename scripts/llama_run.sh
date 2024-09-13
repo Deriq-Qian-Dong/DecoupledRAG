84a5f60c54d33d0add2848258149155c75845ea7
qian <471607065@qq.com>
2024-09-13 12:39:16 +0800

    Update at 2024-09-13 12:39:16

--- scripts/llama_run.sh
+++ scripts/llama_run.sh
@@ -1,10 +1,11 @@
 git pull
 output_dir=output
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 log_dir=${output_dir}/log
 mkdir -p ${output_dir}
 mkdir -p ${log_dir}
 sh scripts/set_gpu_num.sh
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 accelerate launch --main_process_port=29500 --config_file config/default_config.yaml\
+accelerate launch --main_process_port=29500 --config_file config/default_config.yaml\
     src/main.py config/llama_config.yaml\
     | tee ${log_dir}/train.log  
 
