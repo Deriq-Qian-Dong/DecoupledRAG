50fb45ea7da6bd4aae6161cfee9a66938f183e85
qian <471607065@qq.com>
2024-09-19 07:04:34 +0800

    Update at 2024-09-19 07:04:34

--- src/model.py
+++ src/model.py
@@ -328,7 +328,6 @@ class LanguageModelTrainer:
             if 'knowledge_input_ids' in batch:
                 knowledge_outputs, _ = self.compute_hidden_states(batch)
                 batch['knowledge_outputs'] = knowledge_outputs
-                print(knowledge_outputs)
                 batch.pop('knowledge_input_ids')
             forward_time = time()
             outputs = model(**batch)
