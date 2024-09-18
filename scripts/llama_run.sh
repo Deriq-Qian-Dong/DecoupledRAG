3a13f1c586cee3717ae38df7ec342122267899ac
Deriq-Qian-Dong <471607065@qq.com>
2024-09-18 16:25:24 +0800

    Update at 2024-09-18 16:25:24

--- scripts/llama_run.sh
+++ scripts/llama_run.sh
@@ -5,7 +5,7 @@ log_dir=${output_dir}/log
 mkdir -p ${output_dir}
 mkdir -p ${log_dir}
 sh scripts/set_gpu_num.sh
-accelerate launch --main_process_port=29500 --config_file config/default_config.yaml\
+accelerate launch --main_process_port=29511 --config_file config/default_config.yaml\
     src/main.py config/llama_config.yaml\
     | tee ${log_dir}/train.log  
 
