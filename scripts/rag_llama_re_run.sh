255e73752979eb640c3dee9c2e89ff707ac69dc8
Deriq-Qian-Dong <471607065@qq.com>
2024-09-29 08:21:23 +0800

    Update at 2024-09-29 08:21:23

---
+++ scripts/rag_llama_re_run.sh
@@ -0,0 +1,12 @@
+git pull
+export CUDA_VISIBLE_DEVICES=1,2
+output_dir=output_ca_re
+log_dir=${output_dir}/log
+mkdir -p ${output_dir}
+mkdir -p ${log_dir}
+sh scripts/set_gpu_num.sh
+accelerate launch --main_process_port=29501 --config_file config/default_config.yaml\
+    src/main.py config/rag_llama_re_config.yaml\
+    | tee ${log_dir}/train.log  
+
+echo "=================done train=================="
