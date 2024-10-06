356562363098f8c4022c0bef52c1d8d716322233
Deriq-Qian-Dong <471607065@qq.com>
2024-10-06 14:57:03 +0800

    Update at 2024-10-06 14:57:03

---
+++ scripts/llama_re_run.sh
@@ -0,0 +1,12 @@
+git pull
+output_dir=output_sa_re
+export CUDA_VISIBLE_DEVICES=1,2
+log_dir=${output_dir}/log
+mkdir -p ${output_dir}
+mkdir -p ${log_dir}
+sh scripts/set_gpu_num.sh
+accelerate launch --main_process_port=29509 --config_file config/default_config.yaml\
+    src/main.py config/llama_re_config.yaml\
+    | tee ${log_dir}/train.log  
+
+echo "=================done train=================="
