5f1959486168d1dcf49bbbbce9bb835b59efdab8
Deriq-Qian-Dong <471607065@qq.com>
2024-10-06 16:36:44 +0800

    Update at 2024-10-06 16:36:44

--- scripts/rag_llama_mt_run.sh
+++ scripts/rag_llama_mt_run.sh
@@ -1,11 +1,11 @@
 git pull
-export CUDA_VISIBLE_DEVICES=0,1
-output_dir=output_ca
+export CUDA_VISIBLE_DEVICES=2,3
+output_dir=output_ca_mt
 log_dir=${output_dir}/log
 mkdir -p ${output_dir}
 mkdir -p ${log_dir}
 sh scripts/set_gpu_num.sh
-accelerate launch --main_process_port=29501 --config_file config/default_config.yaml\
+accelerate launch --main_process_port=29512 --config_file config/default_config.yaml\
     src/main.py config/rag_llama_mt_config.yaml\
     | tee ${log_dir}/train.log  
 
