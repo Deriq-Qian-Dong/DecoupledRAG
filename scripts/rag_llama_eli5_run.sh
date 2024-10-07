fe3d227b1007cbf2dd248b99f58055d1476f272b
Deriq-Qian-Dong <471607065@qq.com>
2024-10-08 01:06:54 +0800

    Update at 2024-10-08 01:06:54

---
+++ scripts/rag_llama_eli5_run.sh
@@ -0,0 +1,12 @@
+git pull
+export CUDA_VISIBLE_DEVICES=0,1
+output_dir=output_ca_eli5g
+log_dir=${output_dir}/log
+mkdir -p ${output_dir}
+mkdir -p ${log_dir}
+sh scripts/set_gpu_num.sh
+accelerate launch --main_process_port=29511 --config_file config/default_config.yaml\
+    src/main.py config/rag_llama_eli5_config.yaml\
+    | tee ${log_dir}/train.log  
+
+echo "=================done train=================="
