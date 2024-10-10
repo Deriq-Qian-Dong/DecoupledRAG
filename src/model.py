f4c35f2189e9d79ae2eb592f0850f5260d89f222
qian <471607065@qq.com>
2024-10-11 03:08:29 +0800

    Update at 2024-10-11 03:08:29

--- src/model.py
+++ src/model.py
@@ -597,7 +597,8 @@ class LanguageModelTrainer:
         metrics_dict = {metric: {} for metric in metrics}
 
         with torch.no_grad():
-            for number_of_docs in [1,3,5,10,20]:
+            for number_of_docs in [1, 10, 20, 30, 40, 50]:
+            # for number_of_docs in [1,3,5,10,20]:
             # for number_of_docs in [20]:
                 metrics_results_dict = {metric: [] for metric in metrics}
                 self.setup_test_dataloader(number_of_docs=number_of_docs)
