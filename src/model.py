cc9c259fb882469dd8249230ae52fda9cf5ce0a3
Deriq-Qian-Dong <471607065@qq.com>
2024-10-11 00:23:12 +0800

    Update at 2024-10-11 00:23:12

--- src/model.py
+++ src/model.py
@@ -494,7 +494,10 @@ class LanguageModelTrainer:
 
     def generate(self, batch, key):
         model = self.model
-        outputs = model.module.generate(**batch, max_new_tokens=self.config['dataset']['test'][key]['max_new_tokens'], do_sample=False, pad_token_id=self.tokenizer.eos_token_id)
+        if self.config['compare_speed']:
+            outputs = model.module.model.generate(**batch, max_new_tokens=self.config['dataset']['test'][key]['max_new_tokens'], do_sample=False, pad_token_id=self.tokenizer.eos_token_id, min_new_tokens=self.config['dataset']['test'][key]['max_new_tokens'])
+        else:
+            outputs = model.module.generate(**batch, max_new_tokens=self.config['dataset']['test'][key]['max_new_tokens'], do_sample=False, pad_token_id=self.tokenizer.eos_token_id)
         return outputs
     
     def _compute_f1(self, prediction, answer, tokenizer):
