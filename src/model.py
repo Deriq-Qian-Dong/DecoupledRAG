67ebf9cba71c7e0759f3a99ca9b30c61bf54d69b
Deriq-Qian-Dong <471607065@qq.com>
2024-10-10 22:59:36 +0800

    Update at 2024-10-10 22:59:36

--- src/model.py
+++ src/model.py
@@ -570,8 +570,8 @@ class LanguageModelTrainer:
         metrics_dict = {metric: {} for metric in metrics}
 
         with torch.no_grad():
-            # for number_of_docs in [1,3,5,10,20]:
-            for number_of_docs in [20]:
+            for number_of_docs in [1,3,5,10,20]:
+            # for number_of_docs in [20]:
             # for _ in range(1):
                 self.setup_test_dataloader(number_of_docs=number_of_docs)
                 test_dataloaders = self.test_dataloaders
