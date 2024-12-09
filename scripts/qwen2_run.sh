4ce65fb48d1b3b729f31ba3895e09d1ac3170123
Deriq-Qian-Dong <471607065@qq.com>
2024-12-09 19:02:36 +0800

    Update at 2024-12-09 19:02:36

---
+++ scripts/qwen2_run.sh
@@ -0,0 +1,12 @@
+git pull
+output_dir=output_sa_qwen2
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+log_dir=${output_dir}/log
+mkdir -p ${output_dir}
+mkdir -p ${log_dir}
+sh scripts/set_gpu_num.sh
+accelerate launch --main_process_port=29509 --config_file config/default_config.yaml\
+    src/main.py config/qwen2_config.yaml\
+    | tee ${log_dir}/train.log  
+
+echo "=================done train=================="
