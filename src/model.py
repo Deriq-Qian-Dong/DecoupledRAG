2036a0f03bf858f84f0c6adf13c7b07012f537a2
Deriq-Qian-Dong <471607065@qq.com>
2024-12-09 21:49:56 +0800

    Update at 2024-12-09 21:49:56

--- src/model.py
+++ src/model.py
@@ -494,7 +494,7 @@ class LanguageModelTrainer:
             accelerator.backward(loss)
             for name, param in accelerator.unwrap_model(model).model.named_parameters():
                 if param.requires_grad:
-                    print(param)
+                    print(param.grad)
                     # stats[f"grad/{name}"] = param.grad.norm().item()
             backward_time = time() - backward_time
             stats["time/forward"] = forward_time
