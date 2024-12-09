cdd633a66c69b165b1a0b0b0190932afe7bc07ad
Deriq-Qian-Dong <471607065@qq.com>
2024-12-09 17:40:22 +0800

    Update at 2024-12-09 17:40:22

--- transformers/src/transformers/models/qwen2/modeling_qwen2.py
+++ transformers/src/transformers/models/qwen2/modeling_qwen2.py
@@ -1305,7 +1305,7 @@ class Qwen2WithRetrievalHeadAndKnowledgeInjectorForCausalLM(Qwen2PreTrainedModel
             shift_logits = logits[..., :-1, :].contiguous()
             shift_labels = labels[..., 1:].contiguous()
             # Flatten the tokens
-            loss_fct = CrossEntropyLoss(ignore_index=-100)
+            loss_fct = CrossEntropyLoss()
             shift_logits = shift_logits.view(-1, self.config.vocab_size)
             shift_labels = shift_labels.view(-1)
             # Enable model parallelism
